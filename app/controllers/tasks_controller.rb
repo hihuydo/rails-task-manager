be3e18d93c0a219f40bb57b47da23332b86bc7cd
Huy Do <hihuydo@gmail.com>
2021-01-09 17:01:39 +0100

    refactored codes

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,12 @@
 class TasksController < ApplicationController
-  # before_action :find_task, only: [:show, :status]
+  before_action :find_task, only: [:show, :edit, :update, :destroy]
 
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
 
     if @task.completed == true
       @status = "This task is completed"
@@ -30,25 +30,29 @@ class TasksController < ApplicationController
 
   # UPDATE: STEP 1 (GET)
   def edit
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
   end
 
   # UPDATE: STEP 2 (POST)
   def update
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
     @task.update(task_params)
 
     redirect_to task_path
   end
 
   def destroy
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
     @task.destroy
 
     redirect_to tasks_path
   end
 
   private
+
+  def find_task
+    @task = Task.find(params[:id])
+  end
 
   def task_params
     params.require(:task).permit(:title, :details)
