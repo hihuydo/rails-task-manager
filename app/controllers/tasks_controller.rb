487914180ecd05f35605ec3fea9662c30e7a292e
Huy Do <hihuydo@gmail.com>
2021-01-09 15:35:39 +0100

    added some frontend for index and show

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,23 @@
 class TasksController < ApplicationController
+  # before_action :find_task, only: [:show, :status]
+
   def index
     @tasks = Task.all
   end
 
   def show
     @task = Task.find(params[:id])
+
+    if @task.completed == true
+      @test = "This task is completed"
+    else
+      @test = "This task is not completed yet"
+    end
   end
+
+  # private
+
+  # def find_task
+  #   # @task = Task.find(params[:id])
+  # end
 end
